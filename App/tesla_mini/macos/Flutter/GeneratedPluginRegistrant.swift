6746e92a9a95a265c82ffe9fe2c3d94cffa38242
Tenim64 <nathan-b@skynet.be>
2022-11-04 21:48:53 +0100

    Interpreter works?

--- App/tesla_mini/macos/Flutter/GeneratedPluginRegistrant.swift
+++ App/tesla_mini/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,13 @@ import Foundation
 import firebase_core
 import firebase_ml_model_downloader
 import network_info_plus_macos
+import path_provider_macos
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FirebaseModelDownloaderPlugin.register(with: registry.registrar(forPlugin: "FirebaseModelDownloaderPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
