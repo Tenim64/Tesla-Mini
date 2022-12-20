ddd4f644f4111bb65e29332ec0813d0b631b73be
Tenim64 <nathan-b@skynet.be>
2022-12-21 00:46:04 +0100

    Upload

--- Pico files/currentFiles/main.py
+++ Pico files/currentFiles/main.py
@@ -1,11 +1,61 @@
+# -------------------------- Main program --------------------------
+# ---------- Packages ----------
+import json
+from time import sleep
+
+
+# ---------- Variables ----------
+wlan = None
+s = None
+
+
+# ---------- Functions ----------
+def processData(data):
+    blink(1)
+    print("Data received: " + data)
+    data_object = json.loads(data)
+    data_title = data_object["title"]
+    data_data = data_object["data"]
+    if data_title and data_data:
+        if (data_title == "state"):
+            if (data_data == "Testing"):
+                testProcess()
+            if (data_data == "Starting" or data_data == "Stopping"):
+                setPosition(currentPosition)
+
+def testProcess():
+    setPosition(0)
+    sleep(2)
+    setPosition(180)
+    sleep(2)
+    setPosition(90)
+
+
+# ---------- Program ----------
+def main():
+    try:
+        # Set the default turn position
+        setPosition(currentPosition)
+        # Setup the network
+        setupNetwork()
+        # Make a socket
+        makeSocket(wlan)
+        # Listen to the socket
+        listenToSocket(s)
+    except Exception as e:
+        print(e)
+
+
+
 # -------------------------- Servo controller -------------------------- 
 # ---------- Packages ----------
 import machine
 
 
 # ---------- Variables ----------
-led= machine.Pin(25, machine.Pin.OUT)
 servoPin = 1
+
+offset = 10
 
 angleRange = 225
 digitalRange = 180
@@ -14,17 +64,22 @@ currentPosition = 90
 
 
 # ---------- Functions ----------
+def so(x):
+    global offset
+    offset = x
+def sp(x):
+    setPosition(x)
 
 # --- Machine functions ---
 
 # Convert degrees to analog data
 def degreesToAnalog(degrees):
-    analog = round((degrees * 8000 / 225 + 1000) / 50) * 50
+    analog = round((((digitalRange - offset / 2) - degrees) * 8000 / 225 + 1000) / 50) * 50
     return analog
 
 # Convert analog data to degrees
 def analogToDegrees(analog):
-    degrees = round((analog - 1000) * 225 / 8000)
+    degrees = round((digitalRange - offset / 2) - (analog - 1000) * 225 / 8000)
     return degrees
 
 # Turn function using analog data as input
@@ -33,16 +88,15 @@ def turnAnalog(position):
     servo = machine.PWM(machine.Pin(servoPin))
     servo.freq(50)
     global currentPosition
-    currentPosition = degreesToAnalog(min(max(analogToDegrees(position), 0), digitalRange - 1))
+    currentPosition = degreesToAnalog(min(max(analogToDegrees(position), offset), digitalRange - offset))
     servo.duty_u16(currentPosition)
-    print(currentPosition)
-    print(analogToDegrees(currentPosition))
+    print("Turned to ", analogToDegrees(currentPosition), " degrees.")
 
 # --- User functions ---
 
 # Set position
 def setPosition(degrees):
-    position = degreesToAnalog(180 - degrees)
+    position = degreesToAnalog(degrees)
     turnAnalog(position)
 
 # Full left turn
@@ -68,10 +122,6 @@ def turn_right(degrees):
     global currentPosition
     currentPosition = degreesToAnalog(analogToDegrees(currentPosition) - degrees)
     turnAnalog(currentPosition)
-    
-    
-# Default position
-setPosition(currentPosition)
 
 
 
@@ -95,55 +145,60 @@ def blink(blinks):
     sleep(0.2)
   sleep(0.3)
 
-# ---------- Functions ----------
-def checkConnections():
-  print(wlan.ifconfig())
-
 # ---------- Setup network ----------
-print('Setting up network...')
-# Set network type
-wlan = network.WLAN(network.AP_IF)
-# Set the name and password of the WiFi AP
-ssid = "Tesla Mini"
-password = ".'.'.'.'"
-# Create the WiFi AP
-# ip, subnet, gateway, dns
-wlan.ifconfig(('192.168.4.1', '255.255.255.0', '192.168.4.1', '8.8.8.8'))
-wlan.config(essid=ssid, password=password)
-# Start network
-print('Starting network...')
-wlan.active(True)
-# Wait to start
-while wlan.active() == False:
-  wlan.active(True)
-  pass
-print("Acces point active on IP: " + wlan.ifconfig()[0])
-print("Connect to \"" + ssid + "\" using the password \"" + password + "\"")
-blink(2)
+def setupNetwork():
+    global wlan
+    print('Setting up network...')
+    # Set network type
+    wlan = network.WLAN(network.AP_IF)
+    # Set the name and password of the WiFi AP
+    ssid = "Tesla Mini"
+    password = ".'.'.'.'"
+    # Create the WiFi AP
+    # ip, subnet, gateway, dns
+    wlan.ifconfig(('192.168.4.1', '255.255.255.0', '192.168.4.1', '8.8.8.8'))
+    wlan.config(essid=ssid, password=password)
+    # Start network
+    print('Starting network...')
+    wlan.active(True)
+    # Wait to start
+    while wlan.active() == False:
+        wlan.active(True)
+        pass
+    print("Acces point active on IP: " + wlan.ifconfig()[0])
+    print("Connect to \"" + ssid + "\" using the password \"" + password + "\"")
+    blink(2)
 
 # ---------- Connecting ----------
-# Create a socket
-s = socket.socket()
-# Bind the socket to a local address and port
-s.bind((wlan.ifconfig()[0], 80))
-print("Socket bound")
+def makeSocket(wlan):
+    global s
+    # Create a socket
+    s = socket.socket()
+    # Bind the socket to a local address and port
+    s.bind((wlan.ifconfig()[0], 80))
+    print("Socket bound")
 
-# Listen for incoming connections
-s.listen(1)
-print("Listening to socket")
+def listenToSocket(s):
+    # Listen for incoming connections
+    s.listen(1)
+    print("Listening to socket")
 
-# Accept an incoming connection
-conn, addr = s.accept()
-blink(1)
-print("Connection made")
-
-# Continuously receive data and print it
-data = None
-while True:
-  data = conn.recv(1024).decode()
-  if not data:
-    continue
-  if data != None:
-    print("Data received: " + data)
+    # Accept an incoming connection
+    conn, addr = s.accept()
     blink(1)
-  data = None
+    print("Connection made")
+
+    # Continuously receive data and print it
+    data = None
+    while True:
+        data = conn.recv(1024).decode()
+        if not data:
+            continue
+        if data != None:
+            processData(data)
+        data = None
+
+
+
+# -------------------------- Run program --------------------------
+main()
