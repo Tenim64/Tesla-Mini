1104930bf702b549feefee7064dc621e073516d5
Tenim64 <nathan-b@skynet.be>
2022-12-27 16:06:59 +0100

    Changed controls

--- Pico files/ServoController.py
+++ Pico files/ServoController.py
@@ -3,71 +3,96 @@ import machine
 
 
 # ---------- Variables ----------
-led= machine.Pin(25, machine.Pin.OUT)
+# Servo pin number
 servoPin = 1
 
-angleRange = 225
-digitalRange = 180
+# Servo turn values
+analogRange = 225       # [ 0 ; 360 ]
+digitalRange = 180      # [ 0 ; analogRange ]
+analogOffset = 14       # [ 0 ; digitalOffset ]
+digitalOffset = 45      # [ 0 ; analogRange - digitalRange ]
+marginAngle = 50        # [ 0 ; actualRange / 2]
 
-currentPosition = 90
+# Calculated servo turn values
+actualStart_Degrees = digitalOffset + 2 * analogOffset + marginAngle
+actualEnd_Degrees = digitalRange + digitalOffset - marginAngle
+actualRange_Degrees = actualEnd_Degrees - actualStart_Degrees
 
+# Position values
+currentPosition_Percentage = 0      # [ -100 ; 100 ]
 
 # ---------- Functions ----------
-
 # --- Machine functions ---
 
 # Convert degrees to analog data
-def degreesToAnalog(degrees):
-    analog = round((degrees * 8000 / 225 + 1000) / 50) * 50
-    return analog
+def degreesToAnalog(position_Degrees):
+    position_Analog = round((position_Degrees * 8000 / 225 + 1000) / 50) * 50
+    return position_Analog
 
 # Convert analog data to degrees
-def analogToDegrees(analog):
-    degrees = round((analog - 1000) * 225 / 8000)
-    return degrees
+def analogToDegrees(position_Analog):
+    position_Degrees = round((position_Analog - 1000) / 8000 * 225)
+    return position_Degrees
+
+# Convert percentage to degrees
+def percentageToDegrees(position_Percentage):
+    positionProcessed_Degrees = analogRange - ((position_Percentage / 100 + 1) / 2 * actualRange_Degrees + actualStart_Degrees)
+    return positionProcessed_Degrees
+
+# Turn function using percentage as input
+def turnPercentage(position_Percentage):
+    # Position(%) ∈ [ -100 ; 100 ]
+    positionProcessed_Percentage = min(max(position_Percentage, -100), 100)
+
+    # Save new position
+    global currentPosition_Percentage
+    currentPosition_Percentage = positionProcessed_Percentage
+
+    # Convert position from percentage to degrees
+    positionProcessed_Degrees = percentageToDegrees(positionProcessed_Percentage)
+
+    # Convert position from degrees to analog data
+    positionProcessed_Analog = degreesToAnalog(positionProcessed_Degrees)
+    # Turn
+    turnAnalog(positionProcessed_Analog)
+
+
+    print(positionProcessed_Percentage, "% | ", positionProcessed_Degrees, "° | ", positionProcessed_Analog)
 
 # Turn function using analog data as input
-def turnAnalog(position):
+def turnAnalog(position_Analog):
     global servoPin
     servo = machine.PWM(machine.Pin(servoPin))
     servo.freq(50)
-    global currentPosition
-    currentPosition = degreesToAnalog(min(max(analogToDegrees(position), 0), digitalRange - 1))
-    servo.duty_u16(currentPosition)
-    print(currentPosition)
-    print(analogToDegrees(currentPosition))
+    servo.duty_u16(position_Analog)
 
 # --- User functions ---
 
 # Set position
-def setPosition(degrees):
-    position = degreesToAnalog(180 - degrees)
-    turnAnalog(position)
+def setPosition(position_Percentage):
+    turnPercentage(position_Percentage)
 
 # Full left turn
 def full_left():
-    setPosition(angleRange - 1)
+    turnPercentage(-100)
     
 # Full right turn
 def full_right():
-    setPosition(0)
+    turnPercentage(100)
 
 # Turn left n degrees
-def turn_left(degrees):
-    servo = machine.PWM(machine.Pin(servoPin))
-    servo.freq(50)
-    global currentPosition
-    currentPosition = degreesToAnalog(analogToDegrees(currentPosition) + degrees)
-    turnAnalog(currentPosition)
+def turn_left(positionChange_Percentage):
+    global currentPosition_Percentage
+    currentPosition_Percentage -= positionChange_Percentage
+    turnPercentage(currentPosition_Percentage)
 
 # Turn right n degrees
-def turn_right(degrees):
-    servo = machine.PWM(machine.Pin(servoPin))
-    servo.freq(50)
-    global currentPosition
-    currentPosition = degreesToAnalog(analogToDegrees(currentPosition) - degrees)
-    turnAnalog(currentPosition)
-    
+def turn_right(positionChange_Percentage):
+    global currentPosition_Percentage
+    currentPosition_Percentage += positionChange_Percentage
+    turnPercentage(currentPosition_Percentage)
+
+
     
 # Default position
-setPosition(currentPosition)
+setPosition(currentPosition_Percentage)
